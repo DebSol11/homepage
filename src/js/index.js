f32a688e238e8b479905e8e207f3b8ade05dc002
Michael <100490812+DebSol11@users.noreply.github.com>
2026-01-24 11:47:59 +0400

    Add picture credits

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import "../../src/styles.css";
 
+
+// MAXIMIZE FUNCTIONALITY
+// ----------------------
 const fullScreenImg = document.querySelectorAll(".fullscreen-image");
 const maximizeBtn = document.querySelectorAll(".maximize-image-button");
 
 maximizeBtn.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    event.preventDefault();
+    event.preventDefault(); // prevents immediate reloads
     const idValue = btn.id;
     let idNumber = idValue.match(/\d+/g);
     fullScreenImg[idNumber].classList.add("active");
@@ -19,5 +22,3 @@ fullScreenImg.forEach((img) => {
     fullScreenImg[idNumber].classList.remove("active");
   });
 });
-
-console.log(maximizeBtn);
