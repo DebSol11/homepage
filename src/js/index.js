fb418a94553cd190a3d7b0b2744bc169c8e0d1d8
Michael <100490812+DebSol11@users.noreply.github.com>
2026-01-24 11:00:04 +0400

    Work on maximize image functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 import "../../src/styles.css";
+
+const fullScreenImg = document.querySelectorAll(".fullscreen-image");
+const maximizeBtn = document.querySelectorAll(".maximize-image-button");
+
+maximizeBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const idValue = btn.id;
+    fullScreenImg[idValue].classList.add("active");
+  });
+});
+
+fullScreenImg.forEach((img) => {
+  img.addEventListener("click", () => {
+    const idValue = img.id;
+    console.log(idValue);
+    fullScreenImg[idValue].classList.remove("active");
+  });
+});
+
+console.log(maximizeBtn);
